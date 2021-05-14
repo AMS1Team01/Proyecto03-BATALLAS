85c0122b27893431bb5c12e91cf9fc477619b14e
PolTellJove <poltelljove@gmail.com>
2021-05-14 14:12:27 +0200

    Avanzada la mecanica del juego

--- proyecto03 BATALLA/Pol/BattleProject/src/packageBattle/Battle.java
+++ proyecto03 BATALLA/Pol/BattleProject/src/packageBattle/Battle.java
@@ -21,148 +21,64 @@ public class Battle {
 		//Instanciar Random
 		Random randomNumber = new Random();
 		
-		//Se crean las arrays y variables que se van a utilizar
-		Scanner input = new Scanner(System.in);
-		ArrayList<String> menu = new ArrayList<String>();
-		menu.add("Play");
-		menu.add("Choose warrior");
-		menu.add("Choose weapon");
-		menu.add("Exit");
+		Elf pAttacker = new Elf("JoseColetas");
+		Dwarf pDefender = new Dwarf("IwoGorras");
 		
-		//Se pide datos del usuario
-		String nickNamePlayer = input.next();		
-			
-		//Se crea el bot y se le asigna un nombre y un arma aleatorios de las ArrayList warriors y weapons
-		String nickName = "Bot";
-		if (nickName == nickNamePlayer) {
-			nickName = "CPU";
-		}
-		User bot = new User(nickName, allWarriors.get(randomNumber.nextInt(11)), allWeapons.get(randomNumber.nextInt(8)));
-		System.out.println(new String(new char[41]).replace("\0","=") + "BATALLA" + new String(new char[41]).replace("\0","="));
-				
-		//Se crea el usuario
-		nickName = nickNamePlayer;
-		User player = new User(nickName, allWarriors.get(), allWeapons.get(randomNumber.nextInt(8)));
-		ArrayList<User> players = new ArrayList<User>();
-		Warrior pAttacker;
-		Warrior pDefender;
-		while (true) {
-			//Se imprime el menu utilizando una array, porque cuando acaba la primera partida se anyade la opcion de repetir 
-			for (String optionMenu : menu) {
-				System.out.println(menu.indexOf(optionMenu)+1 + ". " + optionMenu);
-			}
-			
-			//Se comprueba lo que pone el usuario. Si no es correcto, sale el mensaje de error correspondiente
-			if (input.hasNextInt()) {
-	        	int optionMenu  = input.nextInt();
-	        	if (optionMenu == 1) {
-	        		System.out.println("\nTu oponente va ser el " + bot.getNickName());
-	        		
-	        		//Se crea el equipo del usuario
-    				if (warrior.race_id == 1) {
-    					player.setWarrior(new Dwarf(input.next()));
-    				} else if (warrior.race_id == 2) {
-    					player.setWarrior(new Human(input.next()));
-    				} else {
-    					player.setWarrior(new Elf(input.next()));
-    				}
-    				
-	    		
-    				//Creamos el equipo del bot
-	        		do {
-	        			int warriorId = (int)(Math.random()*11 + 1);
-	        		}
-	        		while (warriorId == warriorPlayerId);
-	        		
-	    			if (warrior(warriorId).race_id == 1) {
-    					bot.setWarrior(new Dwarf(input.next()));
-	    			} else if (warrior(warriorId).race_id == 2) {
-    					bot.setWarrior(new Human(input.next()));
-    				} else {
-    					bot.setWarrior(new Elf(input.next()));
-    				}
-	    				
+		int turn = 1;
+		System.out.println(new String(new char[47]).replace("\0","=") + "BATALLA" + new String(new char[47]).replace("\0","="));
 
-	    			//Elegimos quien va atacar por el metodo de quien mas velocidad tiene
-	    			if(player.getWarrior().getVelocity() + player.getWeapon().getVelocity() > bot.getWarrior().getVelocity() + bot.getWeapon().getVelocity()) {
-	    				players.add(player);
-	    				players.add(bot);
-	    			} else if(player.getWarrior().getVelocity() + player.getWeapon().getVelocity() < bot.getWarrior().getVelocity() + bot.getWeapon().getVelocity()) {
-	    				players.add(bot);
-	    				players.add(player);
-	    			} else {
-	    				if(player.getWarrior().getAgility() > bot.getWarrior().getAgility()) {
-		    				players.add(player);
-		    				players.add(bot);
-		    			} else if(player.getWarrior().getAgility() < bot.getWarrior().getAgility()) {
-		    				players.add(bot);
-		    				players.add(player);
-		    			} else {
-		    				int numRandom = (int)(Math.random()*2);
-		    				if (numRandom == 1) {
-		    					players.add(player);
-			    				players.add(bot);
-		    				} else {
-		    					players.add(bot);
-			    				players.add(player);
-		    				}
-		    			}
-	    			}
-	    		//Opcion de salir
-	        	}else if(optionMenu == 4) {
-	        		break;
-	        	
-	        	//}else if(optionMenu == 2 && menu.size() == 3) {
-	        		
-	        	//Mensaje de numero incorrecto
-	        	}else {
-	        		System.out.println("No existe la opcion " + optionMenu);
-	        		continue;
-	        	}
-			//Mensaje de que lo que ha puesto el usuario no es un numero
-			}else {
-				System.out.println("Debe ser un numero, la opcion " + input.next() + " no es valida!");
-				continue;
-			}
-						
-			int turn = 0;
-			boolean called1 = false;
-			boolean called2 = false;
-			//Mientras los dos pokemons tienen vida, atacan uno a otro
-			turn++;
+		
+		//Elegimos quien va atacar por el metodo de quien mas velocidad tiene
+		if(pAttacker.getSpeed() > pDefender.getSpeed()) {
 			System.out.println(pDefender.getDefend(pAttacker, pAttacker.getAttack()));
-			System.out.println("\n" + new String(new char[65]).replace("\0","*"));
-			//Comprobamos  que el pokemon defensor  no ha muerto del ataque anterior y puede atacar
-			
-			while (pDefender.getLife() > 0 & pAttacker.getLife() > 0) {
-				turn++;	
-				if (pAttacker.getSpeed() <= pDefender.getSpeed()) {
+		} else if(pAttacker.getSpeed() < pDefender.getSpeed()) {
+			System.out.println(pAttacker.getDefend(pDefender, pDefender.getAttack()));
+		} else {
+			if(pAttacker.getAgility() > pDefender.getAgility()) {
+				System.out.println(pDefender.getDefend(pAttacker, pAttacker.getAttack()));
+			} else if(pAttacker.getAgility() < pDefender.getAgility()) {
+				System.out.println(pAttacker.getDefend(pDefender, pDefender.getAttack()));
+			} else {
+				int numRandom = (int)(Math.random()*2);
+				if (numRandom == 1) {
+					System.out.println(pDefender.getDefend(pAttacker, pAttacker.getAttack()));
+				} else {
+					System.out.println(pAttacker.getDefend(pDefender, pDefender.getAttack()));
+				}
+			}
+		}
+		
+		System.out.println("\n" + new String(new char[65]).replace("\0","*"));
+		
+		//Mientras los dos pokemons tienen vida, atacan uno a otro		
+		while (pDefender.getLife() > 0 & pAttacker.getLife() > 0) {
+			turn++;	
+			if (pAttacker.getSpeed() <= pDefender.getSpeed()) {
+				System.out.println(pAttacker.getDefend(pDefender, pDefender.getAttack()));
+				Warrior aux = pAttacker;
+				pAttacker = pDefender;
+				pDefender = aux;
+			} else {
+				int numRandom = (int)(Math.random()*101);
+				if ((pAttacker.getSpeed() - pDefender.getSpeed())*10 > numRandom) {
+					System.out.println(pDefender.getDefend(pAttacker, pAttacker.getAttack()));
+				} else {
 					System.out.println(pAttacker.getDefend(pDefender, pDefender.getAttack()));
 					Warrior aux = pAttacker;
 					pAttacker = pDefender;
 					pDefender = aux;
-				} else {
-					int numRandom = (int)(Math.random()*101);
-					if ((pAttacker.getSpeed() - pDefender.getSpeed())*10 > numRandom) {
-						System.out.println(pDefender.getDefend(pAttacker, pAttacker.getAttack()));
-					} else {
-						System.out.println(pAttacker.getDefend(pDefender, pDefender.getAttack()));
-						Warrior aux = pAttacker;
-						pAttacker = pDefender;
-						pDefender = aux;
-					}		
-				}
-				System.out.println("\n" + new String(new char[65]).replace("\0","*"));
-			
+				}		
 			}
-				
+			
+			System.out.println("\n" + new String(new char[65]).replace("\0","*"));
+		
 		}
 	
 		//El ganador
-		if (player.getWarrior().getLife() > bot.getWarrior().getLife()) {
-		System.out.println("\nHa ganado " + player.getNickName() + "\n");
+		if (pAttacker.getLife() > pDefender.getLife()) {
+		System.out.println("\nHa ganado " + pAttacker.getName() + "\n");
 		}else {
-		System.out.println("\nHa ganado " + bot.getNickName() + "\n");
+		System.out.println("\nHa ganado " + pDefender.getName() + "\n");
 		}
 		System.out.println("Resum del combat");
 		//Opcio tornar a inici o finalitzar aplicacio
@@ -296,6 +212,8 @@ abstract class Warrior extends WarriorContainer { //Se crea la clase abstracta P
 	public void resetStats() {
 		this.setLife(this.getStartingLife());
 	}
+	
+	
 	
 	public String getDefend(Warrior enemy, int enemyAttack) { //Se crea el metodo getDefend de la interface Defendible
 		
